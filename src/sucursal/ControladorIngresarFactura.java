4f5bd0828e2c276ddd1c3eb97aee7596f9fbef4b
CARLOSBA90 <cepr0190@gmail.com>
2021-02-15 13:21:15 -0300

    ajustando entrada datos en ingreso factura de vista sucursal parte 4

--- src/sucursal/ControladorIngresarFactura.java
+++ src/sucursal/ControladorIngresarFactura.java
@@ -5,6 +5,9 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -13,6 +16,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import modelo.ModeloSucursal;
 import clases.proveedor;
@@ -21,6 +25,7 @@ public class ControladorIngresarFactura implements Initializable {
 	
 	@FXML private DatePicker fecha;
 	@FXML private ComboBox<String> listaDeTipos, listaProveedor, listaCuit;
+	@FXML private TextField subtotal, iva1, iva2, iva3, ivaotros, total = new TextField();
 	
    ObservableList<String> tipo = FXCollections.observableArrayList("A", "B", "C");
    ObservableList<String> proveedor,cuit;
@@ -28,6 +33,9 @@ public class ControladorIngresarFactura implements Initializable {
    
    
 	public void initialize(URL location, ResourceBundle resources) {
+		
+		
+		/// Solicita los datos a la BBDD de nombre de proveedor y su CUIT
 		
 		ModeloSucursal modelo =	 new ModeloSucursal();
 		
@@ -40,7 +48,11 @@ public class ControladorIngresarFactura implements Initializable {
 			e.printStackTrace();
 		}
 		
+		//Genera lista en ArrayList tipo String
+		
 		generarListas(listaProveedores);
+		
+		/// Inserta los datos en cada lista de la GUI
 		
 		listaProveedor.setItems(proveedor);
 		
@@ -48,14 +60,33 @@ public class ControladorIngresarFactura implements Initializable {
 		
 		listaDeTipos.setItems(tipo);
 		
+		///Desactiva el textField total
+		
+		total.setDisable(true);
+		
+		/// Oyente para colocar la fecha como tope hasta el dia actual
+		
 		fecha.setDayCellFactory(picker -> new DateCell() {
 	        public void updateItem(LocalDate date, boolean empty) {
 	            super.updateItem(date, empty);
 	            LocalDate today = LocalDate.now();
-
 	            setDisable(empty || date.compareTo(today) > 0 );
 	        }
 	    });
+		
+		
+		///Oyente para colocar solo numeros dentro de los montos subtotal, impuestos y total
+		
+		ChangeListener<String> soloNumeros = (observable, valorViejo, valorNuevo) -> {
+		    if (!valorNuevo.matches("\\d*(\\.\\d*)?"))
+		      ((StringProperty) observable).set(valorViejo);
+		};
+		
+		subtotal.textProperty().addListener(soloNumeros);
+		iva1.textProperty().addListener(soloNumeros);
+		iva2.textProperty().addListener(soloNumeros);
+		iva3.textProperty().addListener(soloNumeros);
+		ivaotros.textProperty().addListener(soloNumeros);
 	}
 	
 
@@ -85,6 +116,12 @@ public class ControladorIngresarFactura implements Initializable {
     	
     	
     	System.out.println("Ingresaste factura");
+    /*	double valor =Double.parseDouble(subtotal.getText())+
+    			Double.parseDouble(iva1.getText())+
+    			Double.parseDouble(iva2.getText())+
+    			Double.parseDouble(iva3.getText())+
+    			Double.parseDouble(ivaotros.getText());
+    	total.setText(""+valor);*/
     	
     } 
 	
@@ -121,6 +158,13 @@ public class ControladorIngresarFactura implements Initializable {
 		}
 		
 		listaProveedor.getSelectionModel().select(c);
+		
+	}
+	
+	@FXML
+	private void nuevoMonto(ActionEvent e) {
+		
+		//total.setText(""+Double.parseDouble(subtotal.getText()));
 		
 	}
 
